70cb615970bf0f9fb963bf162fe1406f46ad6475
Stacy Crampton <stacyann6284@gmail.com>
2026-03-31 14:39:17 -0400

    try this

--- src/config/db.js
+++ src/config/db.js
@@ -11,11 +11,11 @@ console.log(process.env.DB_PASSWORD);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
 });
 
 pool.on("connect", () => {
